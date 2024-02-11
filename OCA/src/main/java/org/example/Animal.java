757f4e73cfbf454c7e786bcb822bf4af0130fc1f
Sithembiso Gama <sit_gam@rdi.local>
2024-02-11 11:08:26 +0200

    Started wih simple class with 2 variables, getters and setters.

---
+++ OCA/src/main/java/org/example/Animal.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Animal {
+    //Attributes - Variable
+    String name;
+    String species;
+
+    //Behaviours - Methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public void sound() {
+        System.out.println("The sound an animal makes.");
+    }
+}
