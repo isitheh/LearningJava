d6caf700298e5547b51d6a10283656e9b77e215d
Sithembiso Gama <isitheh@gmail.com>
2024-02-13 03:40:19 +0200

    Continue... Instance initializer and constructor.

--- OCA/src/main/java/org/example/transport/Car.java
+++ OCA/src/main/java/org/example/transport/Car.java
@@ -8,6 +8,27 @@ public class Car {
     String make;
     int maxSpeed;
 
+    //Instance initializer block is executed first at run time before the constructor.
+    {
+        System.out.println("instance initializer block invoked");
+    }
+
+    /*
+     * This is a constructor for type Car.
+     * Note: No return type. Constructor name matches class name
+     */
+    public Car() {
+        System.out.println("This is a constructor for type Car.");
+    }
+
+    /*
+     * This is not a constructor for type Car.
+     * Note: This has a return type although class name and method name is a match.
+     */
+    public void Car() {
+        System.out.println("This is not a constructor for type Car.");
+    }
+
     //Methods
     public String getMake() {
         return make;
@@ -24,6 +45,4 @@ public class Car {
     public void setMaxSpeed(int maxSpeed) {
         this.maxSpeed = maxSpeed;
     }
-
-
 }
